3cbc324ea00245f0e4e66114e67d54a97afbae67
Cédric GORMOND <cedric.gormond@gmail.com>
2016-03-22 20:26:08 +0100

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,17 @@
+"""
+Constantes
+"""
+
+# Couleurs
+BLACK   = (   0,   0,   0)
+WHITE   = ( 255, 255, 255)
+BLUE    = (   0,   0, 255)
+ORANGE  = ( 255, 126,   0)
+
+# Dimensions
+SCREEN_WIDTH  = 800
+SCREEN_HEIGHT = 600
+
+# FPS
+
+FPS = 60
