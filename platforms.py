03d29bc8142a56ea1451bbad4814319b47cdee3d
Cédric GORMOND <cedric.gormond@gmail.com>
2016-04-16 23:48:20 +0200

    Update platforms.py

--- platforms.py
+++ platforms.py
@@ -2,55 +2,85 @@
 Module for managing platforms.
 """
 import pygame
-
+ 
 from spritesheet_functions import SpriteSheet
+ 
+# Ces constantes définissent notre platforme
+#   Nomenclature
+#   Location X dans la sprite
+#   Location Y dans la sprite
+#   Largeur de la sprite
+#   HAuteur de la sprite
+ 
 
-# These constants define our platform types:
-#   Name of file
-#   X location of sprite
-#   Y location of sprite
-#   Width of sprite
-#   Height of sprite
-
-GRASS_LEFT            = (576, 720, 70, 70)
-GRASS_RIGHT           = (576, 576, 70, 70)
-GRASS_MIDDLE          = (504, 576, 70, 70)
-STONE_PLATFORM_LEFT   = (432, 720, 70, 40)
-STONE_PLATFORM_MIDDLE = (648, 648, 70, 40)
-STONE_PLATFORM_RIGHT  = (792, 648, 70, 40)
-
-class Platform(pygame.sprite.Sprite):
-    """ Platform the user can jump on """
-
+STONE_AD = (0, 183, 61, 61)
+STONE_BA = (0, 183, 61, 61)
+STONE_BB = (61, 183, 61, 61)
+STONE_BC = (122, 183, 61, 61)
+STONE_BD = (183, 183, 61, 61)
+STONE_CA = (0, 244, 61, 61)
+STONE_CB = (61, 244, 61, 61)
+STONE_CC = (122, 244, 61, 61)
+STONE_CD = (183, 244, 61, 61)
+PLATFORM1G = (366, 0, 61, 61)
+PLATFORM1R = (427, 0, 61, 61)
+D1 = (0, 122, 61, 61)
+class Decors(pygame.sprite.Sprite):
+     
     def __init__(self, sprite_sheet_data):
         """ Platform constructor. Assumes constructed with user passing in
             an array of 5 numbers like what's defined at the top of this
             code. """
-        pygame.sprite.Sprite.__init__(self)
-
-        sprite_sheet = SpriteSheet("data/tiles_spritesheet.png")
+        super().__init__()
+ 
+        sprite_sheet = SpriteSheet("data/tiles_spritesheet_pt.png")
         # Grab the image for this platform
         self.image = sprite_sheet.get_image(sprite_sheet_data[0],
                                             sprite_sheet_data[1],
                                             sprite_sheet_data[2],
                                             sprite_sheet_data[3])
 
+                                            
+    """ Platform the user can jump on """
+    
+class Platform(pygame.sprite.Sprite):
+    """ Platform the user can jump on """
+
+ 
+    def __init__(self, sprite_sheet_data):
+        """ Platform constructor. Assumes constructed with user passing in
+            an array of 5 numbers like what's defined at the top of this
+            code. """
+        super().__init__()
+ 
+        sprite_sheet = SpriteSheet("data/tiles_spritesheet_pt.png")
+        # Grab the image for this platform
+        self.image = sprite_sheet.get_image(sprite_sheet_data[0],
+                                            sprite_sheet_data[1],
+                                            sprite_sheet_data[2],
+                                            sprite_sheet_data[3])
+ 
         self.rect = self.image.get_rect()
-
-
+ 
+ 
 class MovingPlatform(Platform):
     """ This is a fancier platform that can actually move. """
-    change_x = 0
-    change_y = 0
-
-    boundary_top = 0
-    boundary_bottom = 0
-    boundary_left = 0
-    boundary_right = 0
-
-    level = None
-    player = None
-
+ 
+    def __init__(self, sprite_sheet_data):
+ 
+        super().__init__(sprite_sheet_data)
+ 
+        self.change_x = 0
+        self.change_y = 0
+ 
+        self.boundary_top = 0
+        self.boundary_bottom = 0
+        self.boundary_left = 0
+        self.boundary_right = 0
+ 
+        self.level = None
+        self.player = None
+ 
     def update(self):
         """ Move the platform.
             If the player is in the way, it will shove the player
@@ -58,16 +88,17 @@ class MovingPlatform(Platform):
             platform shoves a player into another object. Make sure
             moving platforms have clearance to push the player around
             or add code to handle what happens if they don't. """
-
+ 
+ 
         # Move left/right
         self.rect.x += self.change_x
-
+ 
         # See if we hit the player
         hit = pygame.sprite.collide_rect(self, self.player)
         if hit:
             # We did hit the player. Shove the player around and
             # assume he/she won't hit anything else.
-
+ 
             # If we are moving right, set our right side
             # to the left side of the item we hit
             if self.change_x < 0:
@@ -75,27 +106,28 @@ class MovingPlatform(Platform):
             else:
                 # Otherwise if we are moving left, do the opposite.
                 self.player.rect.left = self.rect.right
-
+ 
         # Move up/down
         self.rect.y += self.change_y
-
+ 
         # Check and see if we the player
         hit = pygame.sprite.collide_rect(self, self.player)
         if hit:
             # We did hit the player. Shove the player around and
             # assume he/she won't hit anything else.
-
+ 
             # Reset our position based on the top/bottom of the object.
             if self.change_y < 0:
                 self.player.rect.bottom = self.rect.top
             else:
                 self.player.rect.top = self.rect.bottom
-
+ 
         # Check the boundaries and see if we need to reverse
         # direction.
         if self.rect.bottom > self.boundary_bottom or self.rect.top < self.boundary_top:
             self.change_y *= -1
-
+ 
         cur_pos = self.rect.x - self.level.world_shift
         if cur_pos < self.boundary_left or cur_pos > self.boundary_right:
             self.change_x *= -1
+
